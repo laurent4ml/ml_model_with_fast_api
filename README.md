67e2526eea3710a00520691a441c9c26f473101c
Laurent <laurent@lanightlife.com>
2023-09-02 22:11:51 -0700

    adding data preparation step to load the dataset into a 'data' folder

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# ML Model with FastAPI 
+# Building a MLOps Pipeline with Github, Github Actions and a Cloud Service Provider
+
+## Preparing Data
+to load the data set just run
+```
+python src/model_build/prepare_data/prepare.py
+```
+
+This will create a folder 'data' and store the dataset in a file census.csv
+
+## Github Actions
+
+We will use Github Actions to:
+- Automate Data Download
+- Check whether the model should be retrained or not
+- Compare models
+- Put a model into production by uploading the new model to a Cloud Provider and deploying it
+- Run tests
+
+To add a github action create a yaml file in .github/workflow
+
+The first Github Action load the census file to WandB artifacts. This is manaual action that should performed first so that the data is available for the next actions.
+Filename: data_download.yaml
+
+## Install 
+
+First you need to install dependencies in conda
+- pip install wandb
---
+++ src/model_build/data_preparation/prepare.py
@@ -0,0 +1,55 @@
+import os
+import requests
+import logging
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
+
+
+def prepare_data(path='data'):
+    # GET CENSUS DATASET
+    logging.info("GET DATASET")
+
+    urls = (
+        "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data",
+        "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.names",
+        "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test",
+    )
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    for url in urls:
+        response = requests.get(url)
+        name = os.path.basename(url)
+        logging.info(f"Processing {name}")
+        with open(os.path.join(path, name), 'wb') as f:
+            f.write(response.content)
+
+    names = [
+        'age',
+        'workclass',
+        'fnlwgt',
+        'education',
+        'education-num',
+        'marital-status',
+        'occupation',
+        'relationship',
+        'race',
+        'sex',
+        'capital-gain',
+        'capital-loss',
+        'hours-per-week',
+        'native-country',
+        'income',
+    ]
+
+    data = pd.read_csv('data/adult.data', names=names)
+
+    # SAVE DATASET INTO CSV FILE
+    logging.info("SAVE DATASET INTO CSV FILE")
+    output_folder = os.path.join(path, "census.csv")
+    data.to_csv(output_folder, sep=",", index=False)
+
+if __name__ == "__main__":
+    prepare_data()
