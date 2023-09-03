f89e3b68e528f1e1d461f5b1bfbe5517758bf2cf
Laurent <laurent@lanightlife.com>
2023-09-03 00:04:09 -0700

    adding step to create an artifact in wandb

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # Building a MLOps Pipeline with Github, Github Actions and a Cloud Service Provider
 
-## Preparing Data
-to load the data set just run
+## Project Setup
+This multiple step project is using python version 3. Setup is done through command line instructions.
+
+## Step 1: Preparing Data
+to load the data set:
 ```
 python src/model_build/prepare_data/prepare.py
 ```
 
 This will create a folder 'data' and store the dataset in a file census.csv
+
+## Step 2: Uploading data set to WandB
+to store a version of the dataset to WandB:
+```
+pip install wandb
+
+export WANDB_API_KEY=<api_key>
+
+python src/model_build/data_preparation/wandb_upload.py --file="./data/census.csv" --artifact_name census_project --artifact_type=dataset --artifact_description="census dataset for ml project"
+```
+
+This will create anew project in WandB and store census.csv as an artifact
 
 ## Github Actions
 
@@ -21,8 +36,3 @@ To add a github action create a yaml file in .github/workflow
 
 The first Github Action load the census file to WandB artifacts. This is manaual action that should performed first so that the data is available for the next actions.
 Filename: data_download.yaml
-
-## Install 
-
-First you need to install dependencies in conda
-- pip install wandb
---
+++ src/model_build/data_preparation/wandb_upload.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import argparse
+import logging
+import wandb
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+def go(args):
+
+    logger.info(f"Starting uploading {args.file} ...")
+
+    with wandb.init(project="census-classification", notes="training census dataset", tags=["training"], job_type="download_data") as run:
+        with open(args.file, 'r') as fp:
+
+            logger.info("Creating artifact")
+            artifact = wandb.Artifact(
+                name=args.artifact_name,
+                type=args.artifact_type,
+                description=args.artifact_description,
+                metadata={'original_file': args.file}
+            )
+            artifact.add_file(fp.name, name='census_clean')
+
+            logger.info("Logging artifact")
+            run.log_artifact(artifact)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download a file and upload it as an artifact to W&B", fromfile_prefix_chars="@"
+    )
+
+    parser.add_argument(
+        "--file", type=str, help="full path to the input file", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_name", type=str, help="Name for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_type", type=str, help="Type for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_description",
+        type=str,
+        help="Description for the artifact",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    go(args)
