f818d0a8285d06f83737e3f752c87bc1825266bf
Laurent <laurent@lanightlife.com>
2023-09-17 22:42:05 -0700

    adding WANDB_API_KEY: secrets.WANDB_API_KEY to github action

--- src/ml/conftest.py
+++ src/ml/conftest.py
@@ -2,8 +2,6 @@ import pytest
 import pandas as pd
 import wandb
 
-# from src.ml.data import process_data
-
 run = wandb.init(project="census-classification", job_type="data_tests")
 
 
@@ -18,25 +16,3 @@ def training_data(request):
         pytest.fail("--input_artifact missing on command line")
     local_path = run.use_artifact(input_artifact).file()
     return pd.read_csv(local_path)
-
-
-"""
-@pytest.fixture(scope="session")
-def training_data(artifact_data):
-    # categorical features
-    cat_features = [
-        "workclass",
-        "education",
-        "marital-status",
-        "occupation",
-        "relationship",
-        "race",
-        "sex",
-        "native-country",
-    ]
-
-    x_train, y_train = process_data(
-        training_data, categorical_features=cat_features, label="income", training=True
-    )
-    return x_train, y_train
-"""
